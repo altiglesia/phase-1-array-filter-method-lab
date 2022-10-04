69d891162cb54fb3f0fa35e103145c5ecd225502
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-03 22:02:15 -0400

    commit tree cce4bf6841ef185283f7a61f64ca208ec54299f9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+// Array.prototype.filter()
+function findMatching(driversArray, name) {
+    return driversArray.filter(
+        (matchingList) => matchingList.toLowerCase() === name.toLowerCase()
+    );
+};
+
+function fuzzyMatch(driversArray, query) {
+    return driversArray.filter(
+        (firstInitials) =>
+        firstInitials.toLowerCase().indexOf(query.toLowerCase()) === 0
+    );
+};
+
+function matchName(driversArray, name) {
+    return driversArray.filter(
+        (driverOrigin) =>
+        driverOrigin.name === name);
+}
+
